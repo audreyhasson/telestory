aad77396efca35f8f1ed36f80331d1be49a087bc
audreyhasson <54117510+audreyhasson@users.noreply.github.com>
2023-01-11 23:02:42 -0500

    Reactions saved after page load

--- components/postContainer.tsx
+++ components/postContainer.tsx
@@ -26,7 +26,23 @@ export default function PostContainer({postArray}) {
         console.log(newState);
     }
 
-    function getPostHTML(content, id, username) {
+    function getUsersReaction(reactionArray) {
+        let reaction = null;
+        if (reactionArray.laughs.includes(userId)) {
+            reaction = 'laughs';
+        } else if (reactionArray.cries.includes(userId)) {
+            reaction = 'cries';
+        } else if (reactionArray.screams.includes(userId)) {
+            reaction = 'screams';
+        } else if (reactionArray.plots.includes(userId)) {
+            reaction = 'plots';
+        } else if (reactionArray.crafts.includes(userId)) {
+            reaction = 'crafts';
+        }
+        return reaction
+    }
+
+    function getPostHTML(content, id, username, reaction) {
         return (
             <div className={styles.post}>
                 <div className={styles.postContent}>
@@ -35,11 +51,12 @@ export default function PostContainer({postArray}) {
                 <div className={styles.postBar + " postForceStyle"}>
                     <p className="text-xl">{username}</p>
                     <div id={id} className={styles.reactionContainer}>
-                        <button onClick={handleReactionClick} id="laughs">i laughed</button>
-                        <button onClick={handleReactionClick} id="cries">i cried</button>
-                        <button onClick={handleReactionClick} id="screams">i screamed</button>
-                        <button onClick={handleReactionClick} id="plots">dope plot</button>
-                        <button onClick={handleReactionClick} id="crafts">great wordwork</button>
+                        <button onClick={handleReactionClick} className={reaction=='laughs' ?'clicked' : ''} id="laughs">
+                            i laughed</button>
+                        <button onClick={handleReactionClick} className={reaction=='cries' ?'clicked' : ''} id="cries">i cried</button>
+                        <button onClick={handleReactionClick} className={reaction=='screams' ?'clicked' : ''} id="screams">i screamed</button>
+                        <button onClick={handleReactionClick} className={reaction=='plots' ?'clicked' : ''} id="plots">dope plot</button>
+                        <button onClick={handleReactionClick} className={reaction=='crafts' ?'clicked' : ''} id="crafts">great wordwork</button>
                     </div>
                 </div>
             </div>
@@ -53,12 +70,14 @@ export default function PostContainer({postArray}) {
     for (let i=0; i<(postArray.length-1); i=i+2){
         const content = postArray[i].text;
         const id = postArray[i]._id;
+        const reaction = getUsersReaction(postArray[i].reactions);
         const username = postArray[i].author ? postArray[i].author : "anonymous";
-        const postHTML1 = getPostHTML(content, id, username)
+        const postHTML1 = getPostHTML(content, id, username, reaction)
         const content2 = postArray[i+1].text;
         const id2 = postArray[i+1]._id;
         const username2 = postArray[i+1].author ? postArray[i+1].author : "anonymous";
-        const postHTML2 = getPostHTML(content2, id2, username2)
+        const reaction2 = getUsersReaction(postArray[i+1].reactions);
+        const postHTML2 = getPostHTML(content2, id2, username2, reaction2)
         
         posts.push(
             <tr>
@@ -73,7 +92,8 @@ export default function PostContainer({postArray}) {
         const content = postArray[postArray.length-1].text;
         const id = postArray[postArray.length-1]._id;
         const username = postArray[postArray.length-1].author ? postArray[postArray.length-1].author : "anonymous";
-        const postHTML = getPostHTML(content, id, username)
+        const reaction = getUsersReaction(postArray[postArray.length-1].reactions);
+        const postHTML = getPostHTML(content, id, username, reaction)
         posts.push(
             <tr>
                 <td className="w-1/2">{postHTML}</td>
